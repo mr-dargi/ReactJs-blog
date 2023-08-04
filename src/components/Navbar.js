ea8dffbfe5436962318309de547726ec80d8ba42
mr-dargi <dargimohammadreza@gmail.com>
2023-08-04 13:39:10 +0330

    Adding new components Navbar.js and Home.js to the project

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>The Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  )
+}
