c31dd97a451ad5d3f5ce57fc9715cb907753fb96
mr-dargi <dargimohammadreza@gmail.com>
2023-08-06 10:43:46 +0330

    Adding data to our Home.js component for creating blog post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [blogs, setBlogs] = useState([
+    {id: 1,title: "My new website", body: "lorem ipsum...", author: "Mohammad Reza Dargi"},
+    {id: 2,title: "Welcom party", body: "lorem ipsum...", author: "Amir Mohammad"},
+    {id: 3,title: "Web dev top tips", body: "lorem ipsum...", author: "Jadi"}
+  ])
+
+  const blogPost = blogs.map(blog => {
+    return (
+      <div className="blog-preview" key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+      </div>
+    )
+  })
+
+
   return (
     <div className="home">
-      <h2>Home Page</h2>
+      {blogPost}
     </div>
   )
 }
