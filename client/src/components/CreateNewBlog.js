b239929c6f31df0b434e9f9188620140f0b4bebf
mr-dargi <dargimohammadreza@gmail.com>
2023-08-14 18:14:14 +0330

    Creating form inside CreateNewBlog.js file and adding a some css style to it

--- client/src/components/CreateNewBlog.js
+++ client/src/components/CreateNewBlog.js
@@ -1,5 +1,25 @@
 export default function CreateNewBlog() {
   return (
-    <div>hello</div>
+   <div className="create">
+    <h2>Add a New Blog</h2>
+    <form>
+      <label>Add a New Blog:</label>
+      <input
+        type="text"
+        required
+      />
+      <label>Blog body:</label>
+      <textarea
+        required
+      ></textarea>
+      <label>Blog author:</label>
+      <select>
+        <option value="Mohammad Reza Dargi">Mohammad Reza Dargi</option>
+        <option value="Amir Mohammad">Amir Mohammad</option>
+        <option value="Jadi">Jadi</option>
+      </select>
+      <button>Add Blog</button>
+    </form>
+   </div>
   )
 }
