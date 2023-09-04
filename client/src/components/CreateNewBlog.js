19b1bd008aba8aeeb597001181e9f1d91a04bcdc
mr-dargi <dargimohammadreza@gmail.com>
2023-09-04 12:23:05 +0330

    Add useState and handleChange function inside CreateNewBlog.js file for save and receive data from input, textarea and select

--- client/src/components/CreateNewBlog.js
+++ client/src/components/CreateNewBlog.js
@@ -1,4 +1,24 @@
+import { useState } from "react"
+
 export default function CreateNewBlog() {
+  const [formData, setFormData] = useState({
+    title: "",
+    body: "",
+    author: ""
+  });
+
+  const handleChange = event => {
+
+    const { name, value } = event.target;
+
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  console.log(formData);
+
   return (
    <div className="create">
     <h2>Add a New Blog</h2>
@@ -7,13 +27,24 @@ export default function CreateNewBlog() {
       <input
         type="text"
         required
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
       />
       <label>Blog body:</label>
       <textarea
         required
+        name="body"
+        value={formData.body}
+        onChange={handleChange}
       ></textarea>
       <label>Blog author:</label>
-      <select>
+      <select
+        name="author"
+        value={formData.author}
+        onChange={handleChange}
+      >
+        <option value="">-- Choose Option --</option>
         <option value="Mohammad Reza Dargi">Mohammad Reza Dargi</option>
         <option value="Amir Mohammad">Amir Mohammad</option>
         <option value="Jadi">Jadi</option>
